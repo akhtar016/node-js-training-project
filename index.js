b08ab7b9e55b4ef64c6cede3f06599cc60b52410
akhtar016 <akhtaruzzamankhan016@gmail.com>
2020-08-05 22:18:08 +0600

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.post('/submit', (req, res) => {
+   console.log("Request is successfully sent.")
+   
+})
+
 app.listen(port,'127.0.0.1',  ()=> {
     console.log(`Listening to port ${port}`)
 })
